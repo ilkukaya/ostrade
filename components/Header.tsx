813213552593f82a5cadd2c9f8012bcf40171577
hepcode <57993337+ilkukaya@users.noreply.github.com>
2026-09-19 20:30:11 +0300

    feat: rebrand header as OSTRADE and add theme control

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
-import Image from "next/image";
 import NavItems from "@/components/NavItems";
 import UserDropdown from "@/components/UserDropdown";
-import {searchStocks} from "@/lib/actions/finnhub.actions";
+import BrandWordmark from "@/components/BrandWordmark";
+import ThemeToggle from "@/components/ThemeToggle";
+import { searchStocks } from "@/lib/actions/finnhub.actions";
 
 const Header = async ({ user }: { user: User }) => {
     const initialStocks = await searchStocks();
@@ -10,21 +10,19 @@ const Header = async ({ user }: { user: User }) => {
     return (
         <header className="sticky top-0 header">
             <div className="container header-wrapper">
-                <Link href="/" className="flex items-center justify-center gap-2">
-                    <Image
-                        src="/assets/images/logo.png"
-                        alt="OpenStock"
-                        width={200}
-                        height={50}
-                    />
-                </Link>
+                <BrandWordmark />
                 <nav className="hidden sm:block">
-                    <NavItems initialStocks={initialStocks}/>
+                    <NavItems initialStocks={initialStocks} />
                 </nav>
-
-                <UserDropdown user={user} initialStocks={initialStocks} />
+                <div className="flex items-center gap-2">
+                    <div className="hidden lg:block">
+                        <ThemeToggle compact />
+                    </div>
+                    <UserDropdown user={user} initialStocks={initialStocks} />
+                </div>
             </div>
         </header>
-    )
-}
-export default Header
+    );
+};
+
+export default Header;
